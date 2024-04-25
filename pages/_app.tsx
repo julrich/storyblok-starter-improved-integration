ee4392403fa60f4183a1656f43b9eb8f0bc61153
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-04-25 15:56:41 +0200

    fix: try with localFont

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
 import DsaProviders from "@kickstartds/ds-agency/providers";
 import { Header } from "@kickstartds/ds-agency/header";
 import { Footer } from "@kickstartds/ds-agency/footer";
@@ -21,6 +23,75 @@ initStoryblok(process.env.NEXT_STORYBLOK_API_TOKEN);
 if (typeof window !== "undefined") {
   console.log(palette);
 }
+
+const fonts = [
+  localFont({
+    src: [
+      { path: "/fonts/Fredoka-Regular.woff2", weight: "400", style: "normal" },
+    ],
+    preload: true,
+    display: "optional",
+    declarations: [
+      {
+        prop: "font-family",
+        value: "Fredoka",
+      },
+    ],
+  }),
+  localFont({
+    src: [
+      { path: "/fonts/Fredoka-Regular.woff", weight: "400", style: "normal" },
+      { path: "/fonts/Fredoka-SemiBold.woff2", weight: "600", style: "normal" },
+      { path: "/fonts/Fredoka-SemiBold.woff", weight: "600", style: "normal" },
+      { path: "/fonts/Fredoka-Medium.woff2", weight: "500", style: "normal" },
+      { path: "/fonts/Fredoka-Medium.woff", weight: "500", style: "normal" },
+      { path: "/fonts/Fredoka-Light.woff2", weight: "300", style: "normal" },
+      { path: "/fonts/Fredoka-Light.woff", weight: "300", style: "normal" },
+      { path: "/fonts/Fredoka-Bold.woff2", weight: "bold", style: "normal" },
+      { path: "/fonts/Fredoka-Bold.woff", weight: "bold", style: "normal" },
+    ],
+    preload: false,
+    display: "optional",
+    declarations: [
+      {
+        prop: "font-family",
+        value: "Fredoka",
+      },
+    ],
+  }),
+  localFont({
+    src: [
+      { path: "/fonts/Mulish-Regular.woff2", weight: "400", style: "normal" },
+    ],
+    preload: true,
+    display: "optional",
+    declarations: [
+      {
+        prop: "font-family",
+        value: "Mulish",
+      },
+    ],
+  }),
+  localFont({
+    src: [
+      { path: "/fonts/Mulish-Regular.woff", weight: "400", style: "normal" },
+      { path: "/fonts/Mulish-600.woff2", weight: "600", style: "normal" },
+      { path: "/fonts/Mulish-600.woff", weight: "600", style: "normal" },
+      { path: "/fonts/Mulish-800.woff2", weight: "800", style: "normal" },
+      { path: "/fonts/Mulish-800.woff", weight: "800", style: "normal" },
+      { path: "/fonts/Mulish-300.woff2", weight: "300", style: "normal" },
+      { path: "/fonts/Mulish-300.woff", weight: "300", style: "normal" },
+    ],
+    preload: false,
+    display: "optional",
+    declarations: [
+      {
+        prop: "font-family",
+        value: "Mulish",
+      },
+    ],
+  }),
+];
 
 export default function App({
   Component,
